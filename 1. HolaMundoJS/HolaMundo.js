74aadb347b567e8e6955d037988aad4a43111202
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-06 16:10:32 +0200

    Add files via upload

---
+++ 1. HolaMundoJS/HolaMundo.js
@@ -0,0 +1,6 @@
+// "Hello world!"
+// En JS usamos console.log para imprimir en la consola
+// Es el equivalente a System.out.println en Java o print en Python
+
+console.log("Hola mundo!");
+console.log("Este es nuestro primer programa en JavaScript");
